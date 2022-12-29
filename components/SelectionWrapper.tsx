8baf85e09ee8f6af61fe77b03abcce04b9d12b19
Joshua Javier <joshua.javier.github@gmail.com>
2022-12-29 14:19:18 +0800

    Updated color schemes

--- components/SelectionWrapper.tsx
+++ components/SelectionWrapper.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 import { getValidCharacters } from "@utils/helpers";
 
 const validCharacters = getValidCharacters();
-const baseRedButton =
+const baseButton =
   "py-1 px-3 border-2 border-neutral-400 rounded-md font-bold shadow-md h-[35px]";
 
 function filterCharactersList(query: string) {
@@ -48,12 +48,12 @@ function SelectionWrapper() {
         <div id="hydro-logo"></div>
         <div id="pyro-logo"></div>
       </div> */}
-      <div className="my-4 flex gap-4 items-center justify-center">
+      <div className="flex flex-col md:flex-row gap-4 my-4 items-center justify-center">
         <button
           className={
             excludedCharacterIds.length === validCharacters.length
-              ? baseRedButton + " bg-blue-900 hover:bg-blue-800"
-              : baseRedButton + " bg-red-900 hover:bg-red-800"
+              ? baseButton + " bg-white hover:bg-white text-black"
+              : baseButton + " bg-gray-900 hover:bg-gray-800  text-white"
           }
           onClick={handleClick}
         >
@@ -61,7 +61,7 @@ function SelectionWrapper() {
             ? "Select All"
             : "Deselect All"}
         </button>
-        <div className="flex gap-1 h-[35px] rounded-md border-2 border-neutral-400 bg-indigo-900 py-1 px-2 drop-shadow-lg focus-within:shadow focus-within:shadow-slate-200 focus-within:drop-shadow-none">
+        <div className="flex gap-1 h-[35px] rounded-md border-2 border-neutral-400 bg-gray-900 py-1 px-2 drop-shadow-lg focus-within:shadow focus-within:shadow-slate-200 focus-within:drop-shadow-none">
           <div className="mr-2">
             <Image
               alt="search_icon"
@@ -74,7 +74,7 @@ function SelectionWrapper() {
             type="text"
             name="character-search"
             id="character-search"
-            className="bg-indigo-900 border-none outline-none"
+            className="bg-gray-900 border-none outline-none"
             placeholder="Search character"
             aria-label="search character"
             onChange={(e) => setQuery(e.target.value)}
